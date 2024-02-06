678946e397f2ed6092c432c74d3c988a4fd70c22
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-06 22:43:54 +0600

    login and signup page render

--- text_retrieval/urls.py
+++ text_retrieval/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('login_success/', views.login_success, name='login_success'),
+    path('signup/', views.signup, name='signup'),
+    path('save_signup_info/', views.save_signup_info, name='save_signup_info'),
     path('text_analyze/', views.text_analyze, name='text_analyze'),
     path('text_analyze/chat/', views.chat, name='chat'),
     path('response_history/', views.response_history, name='response_history'),
--- text_retrieval/views.py
+++ text_retrieval/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# @login_required(login_url='login_page')
 def index(request):
     return render(request, 'index.html')
 
@@ -12,3 +14,15 @@ def chat(request):
 
 def response_history(request):
     return render(request=request, template_name='table.html')
+
+def login(request):
+    return render(request=request, template_name='login.html')
+
+def signup(request):
+    return render(request=request, template_name='signup.html')
+
+def save_signup_info(request):
+    return redirect('login')
+
+def login_success(request):
+    return redirect('index')
