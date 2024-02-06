6b9d34f36c38491edcd332076872e9fd98a49d09
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-07 02:56:15 +0600

    user signup and authentication

--- text_retrieval/urls.py
+++ text_retrieval/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('login_success/', views.login_success, name='login_success'),
-    path('signup/', views.signup, name='signup'),
-    path('save_signup_info/', views.save_signup_info, name='save_signup_info'),
+    path('login/', views.login_function, name='login'),
+    path('signup/', views.signup_function, name='signup'),
+    path('logout/', views.logout_function, name='logout'),
     path('text_analyze/', views.text_analyze, name='text_analyze'),
     path('text_analyze/chat/', views.chat, name='chat'),
     path('response_history/', views.response_history, name='response_history'),
--- text_retrieval/views.py
+++ text_retrieval/views.py
@@ -1,28 +1,80 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login as loginUser, logout
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
-# @login_required(login_url='login_page')
+
+@login_required(login_url='login')
 def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
     return render(request, 'index.html')
 
+@login_required(login_url='login')
 def text_analyze(request):
     return render(request=request, template_name='text_analyze.html')
 
+@login_required(login_url='login')
 def chat(request):
     return render(request=request, template_name='chat.html')
 
+@login_required(login_url='login')
 def response_history(request):
     return render(request=request, template_name='table.html')
 
-def login(request):
-    return render(request=request, template_name='login.html')
+def login_function(request):
+    if request.method == 'GET':
+        print("inside login GET")
+        form = AuthenticationForm()
+        context = {
+            "form": form
+        }
+        return render(request=request, template_name='login.html', context=context)
 
-def signup(request):
-    return render(request=request, template_name='signup.html')
+    else:
+        print("inside login POST")
+        form = AuthenticationForm(data=request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
 
-def save_signup_info(request):
+            print(username)
+            print(password)
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                loginUser(request, user)
+                return redirect('index')
+        else:
+            context = {
+                "form": form
+            }
+            return render(request=request, template_name='login.html', context=context)
+
+def signup_function(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            'form': form
+        }
+        return render(request=request, template_name='signup.html', context=context)
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        context = {
+            'form': form
+        }
+        
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                return redirect('login')
+        else:
+            return render(request=request, template_name='signup.html', context=context)
+        
+def logout_function(request):
+    logout(request=request)
     return redirect('login')
-
-def login_success(request):
-    return redirect('index')
