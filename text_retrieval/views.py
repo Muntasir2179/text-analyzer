13bf0ee1c50ab0e0707f1d07f6b38be1f3fed290
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-07 14:44:34 +0600

    user authentication with proper error messages

--- text_retrieval/views.py
+++ text_retrieval/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login as loginUser, logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
@@ -26,7 +27,6 @@ def response_history(request):
 
 def login_function(request):
     if request.method == 'GET':
-        print("inside login GET")
         form = AuthenticationForm()
         context = {
             "form": form
@@ -34,23 +34,26 @@ def login_function(request):
         return render(request=request, template_name='login.html', context=context)
 
     else:
-        print("inside login POST")
+        error_message = None
         form = AuthenticationForm(data=request.POST)
-        print(form.is_valid())
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-
-            print(username)
-            print(password)
-
             user = authenticate(username=username, password=password)
             if user is not None:
                 loginUser(request, user)
                 return redirect('index')
         else:
+            username = request.POST.get('username')
+            all_user = User.objects.all()
+            error_message = "No user exit with this username"
+            for single_user in all_user:
+                if username == single_user.username:
+                    error_message = "Password doesn't match"
+
             context = {
-                "form": form
+                "form": form,
+                "error_message": error_message
             }
             return render(request=request, template_name='login.html', context=context)
 
@@ -64,17 +67,44 @@ def signup_function(request):
     
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        # forming error messages
+        error_messages = check_user_credentials({
+            'username': request.POST.get('username'),
+            'password1': request.POST.get('password1'),
+            'password2': request.POST.get('password2')
+        })
         context = {
-            'form': form
+                'form': form,
+                'error_messages': error_messages
         }
-        
-        if form.is_valid():
-            user = form.save()
-            if user is not None:
-                return redirect('login')
+        if len(error_messages.keys()) == 0:
+            if form.is_valid():
+                user = form.save()
+                if user is not None:
+                    return redirect('login')
         else:
             return render(request=request, template_name='signup.html', context=context)
         
 def logout_function(request):
     logout(request=request)
     return redirect('login')
+
+
+def check_user_credentials(user_info):
+    username = user_info['username']
+    password1 = user_info['password1']
+    password2 = user_info['password2']
+
+    all_user = User.objects.all()
+    error_messages = {}
+    for single_user in all_user:
+        if username == single_user.username:
+            error_messages['username_error_msg'] = "Username is taken"
+
+    if password1 != password2:
+        error_messages['password_error_msg'] = "Password doesn't match"
+    else:
+        if len(password1) < 8:
+            error_messages['password_error_msg'] = "Minimum password length is 8"
+    
+    return error_messages
