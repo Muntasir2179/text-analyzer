7271608ab61083eeb7758f4d24f64e851c7711f5
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-10 00:59:54 +0600

    similarity search and chatting

--- text_retrieval/urls.py
+++ text_retrieval/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('logout/', views.logout_function, name='logout'),
     path('text_analyze/', views.text_analyze, name='text_analyze'),
     path('text_analyze/chat/', views.chat, name='chat'),
-    path('response_history/', views.response_history, name='response_history'),
 ]
--- text_retrieval/views.py
+++ text_retrieval/views.py
@@ -23,6 +23,7 @@ def index(request):
     vector_operations.create_text_collection()  # creating a collection for the authenticated user
     return render(request=request, template_name='index.html', context={'current_user': request.user})
 
+
 @login_required(login_url='login')
 def text_analyze(request):
     if request.method == 'POST':
@@ -40,17 +41,27 @@ def text_analyze(request):
             document_chunks = get_document_chunks()
             # inserting the chunks into the collection
             vector_operations.insert_documents(documents=document_chunks)
+            # listing all the files in 'uploads' folder and deleting those uploaded files
+            file_list = os.listdir(BASE_DIR / 'uploads')
+            if len(file_list) != 0:
+                for file_name in file_list:
+                    os.remove(os.path.join(BASE_DIR / 'uploads', file_name))
         return render(request=request, template_name='chat.html', context={'current_user': request.user})
     else:
         return render(request=request, template_name='text_analyze.html', context={'current_user': request.user})
 
-@login_required(login_url='login')
-def chat(request):
-    return render(request=request, template_name='chat.html', context={'current_user': request.user})
 
 @login_required(login_url='login')
-def response_history(request):
-    return render(request=request, template_name='table.html', context={'current_user': request.user})
+def chat(request):
+    context = {
+        'current_user': request.user
+    }
+    if request.method == 'POST':
+        query_text = request.POST.get('query')  # fetching the query
+        query_response = vector_operations.query(query=query_text)  # performing vector search 
+        context['query_response'] = query_response if query_response is not None else "No response"
+    return render(request=request, template_name='chat.html', context=context)
+
 
 def login_function(request):
     if request.method == 'GET':
@@ -87,6 +98,7 @@ def login_function(request):
             }
             return render(request=request, template_name='login.html', context=context)
 
+
 def signup_function(request):
     if request.method == 'GET':
         # if authenticated user tries to access the signup url again then redirect to home page
@@ -117,7 +129,8 @@ def signup_function(request):
                     return redirect('login')
         else:
             return render(request=request, template_name='signup.html', context=context)
-        
+
+
 def logout_function(request):
     # listing all the files in 'uploads' folder and deleting those uploaded files
     file_list = os.listdir(BASE_DIR / 'uploads')
